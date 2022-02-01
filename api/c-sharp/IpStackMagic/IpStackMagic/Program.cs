760d9c9edbc921b962393460ba4336fbbe0042a2
dr. joshua butler <josh.butler929@gmail.com>
2022-02-01 13:19:23 -0500

    Add API example call to IP Stack

---
+++ api/c-sharp/IpStackMagic/IpStackMagic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace IpStackMagic
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "http://api.ipstack.com/check?access_key=";
+            HttpClient client = new HttpClient();
+
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if(response.IsSuccessStatusCode)
+            {
+                string msg = await response.Content.ReadAsStringAsync();
+                dynamic info = JsonConvert.DeserializeObject<dynamic>(msg);
+                Console.WriteLine(info);
+                Console.WriteLine($"Hello user, you are connecting from {info.region_code}");
+            } else
+            {
+                Console.WriteLine("No bueno!");
+                Console.WriteLine(response.StatusCode);
+            }
+            
+
+        }
+    }
+}
